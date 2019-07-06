a5e2de64e2c5ea8e3a8704019b3798fdb3e8cb72
unknown <lifeattickville@gmail.com>
2019-07-06 14:07:57 -0400

    Headers; cleanup

--- src/Screens/Debug/AddBuilding.cs
+++ src/Screens/Debug/AddBuilding.cs
@@ -1,7 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// CivOne
+//
+// To the extent possible under law, the person who associated CC0 with
+// CivOne has waived all copyright and related or neighboring rights
+// to CivOne.
+//
+// You should have received a copy of the CC0 legalcode along with this
+// work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+// Author: Kevin Routley : July, 2019
+
+using System;
 using System.Linq;
-using System.Text;
 using CivOne.Buildings;
 using CivOne.Enums;
 using CivOne.Graphics;
