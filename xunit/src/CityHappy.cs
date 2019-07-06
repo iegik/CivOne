061599920d3ae067fe3689db97087d5c5552954d
unknown <lifeattickville@gmail.com>
2019-07-06 14:07:31 -0400

    More tests; headers; cleanup

--- xunit/src/CityHappy.cs
+++ xunit/src/CityHappy.cs
@@ -1,10 +1,34 @@
-﻿using CivOne.src;
+﻿// CivOne
+//
+// To the extent possible under law, the person who associated CC0 with
+// CivOne has waived all copyright and related or neighboring rights
+// to CivOne.
+//
+// You should have received a copy of the CC0 legalcode along with this
+// work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+// Author: Kevin Routley : July, 2019
+
+using CivOne.src;
 using System.Linq;
+using CivOne.Buildings;
 using Xunit;
 
 namespace CivOne.UnitTests
 {
 
+    /// <summary>
+    /// Tests to exercise City citizen happiness. Citizen happiness
+    /// is displayed in the 'Happy' pane of the City manager view
+    /// as a five-step sequence:
+    /// 1. initial state - determined by game difficulty
+    /// 2. impact of luxuries - from entertainers or settings
+    /// 3. impact of buildings - temple, etc
+    /// 4. impact of units - presence or absence of units depending on government
+    /// 5. impact of wonders
+    /// The final results are displayed in the 'header' of the City manager
+    /// view and dictate if a city goes into disorder or celebration.
+    /// </summary>
     public class CityHappy : TestsBase
     {
         /// <summary>
@@ -255,6 +279,47 @@ namespace CivOne.UnitTests
         }
 
         /// <summary>
+        /// City size 4, King: temple and no other changes means 4 content
+        /// </summary>
+        [Fact]
+        public void City4Temple()
+        {
+            var unit = Game.Instance.GetUnits().First(x => x.Owner == playa.Civilization.Id);
+            City acity = Game.Instance.AddCity(playa, 1, unit.X, unit.Y);
+            acity.Size = 4;
+            acity.ResetResourceTiles(); // setting city size doesn't allocate all resources
+
+            acity.AddBuilding(Reflect.GetBuildings().First(b => b is Temple));
+
+            using (var foo = acity.Residents.GetEnumerator())
+            {
+                foo.MoveNext();
+                var citizenTypes = foo.Current;
+                // initial state
+                Assert.Equal(0, citizenTypes.happy);
+                Assert.Equal(3, citizenTypes.content);
+                Assert.Equal(1, citizenTypes.unhappy);
+
+                foo.MoveNext();
+                citizenTypes = foo.Current;
+                // no luxury effect
+                Assert.Equal(0, citizenTypes.happy);
+                Assert.Equal(3, citizenTypes.content);
+                Assert.Equal(1, citizenTypes.unhappy);
+                Assert.Equal(0, citizenTypes.elvis);
+
+                foo.MoveNext();
+                citizenTypes = foo.Current;
+                // temple effect
+                Assert.Equal(0, citizenTypes.happy);
+                Assert.Equal(4, citizenTypes.content);
+                Assert.Equal(0, citizenTypes.unhappy);
+                Assert.Equal(0, citizenTypes.elvis);
+            }
+
+        }
+
+        /// <summary>
         /// First tricky one. City size 6 at King level: starts with 3 content, 3 unhappy.
         /// Switch 3 people to entertainers: now 3 unhappy, 3 entertainers. Entertainers
         /// make content people happy, then unhappy people content, but sequentially.
@@ -265,7 +330,7 @@ namespace CivOne.UnitTests
         /// that is not how Microprose did it.
         /// </summary>
         [Fact]
-        public void CityHappy6With3Entertainers()
+        public void City6With3EntertainersAndTemple()
         {
             var unit = Game.Instance.GetUnits().First(x => x.Owner == playa.Civilization.Id);
             City acity = Game.Instance.AddCity(playa, 1, unit.X, unit.Y);
@@ -275,6 +340,7 @@ namespace CivOne.UnitTests
             MakeOneEntertainer(acity);
             MakeOneEntertainer(acity);
             MakeOneEntertainer(acity);
+            acity.AddBuilding(Reflect.GetBuildings().First(b => b is Temple));
 
             using (var foo = acity.Residents.GetEnumerator())
             {
@@ -290,9 +356,95 @@ namespace CivOne.UnitTests
                 Assert.Equal(1, citizenTypes.content);
                 Assert.Equal(1, citizenTypes.unhappy);
                 Assert.Equal(3, citizenTypes.elvis);
+
+                foo.MoveNext();
+                citizenTypes = foo.Current;
+                // temple effect
+                Assert.Equal(1, citizenTypes.happy);
+                Assert.Equal(2, citizenTypes.content);
+                Assert.Equal(0, citizenTypes.unhappy);
+                Assert.Equal(3, citizenTypes.elvis);
             }
 
         }
 
+        [Fact]
+        public void City5With1EntAndTemple()
+        {
+            var unit = Game.Instance.GetUnits().First(x => x.Owner == playa.Civilization.Id);
+            City acity = Game.Instance.AddCity(playa, 1, unit.X, unit.Y);
+            acity.Size = 5;
+            acity.ResetResourceTiles(); // setting city size doesn't allocate all resources
+
+            MakeOneEntertainer(acity);
+            acity.AddBuilding(Reflect.GetBuildings().First(b => b is Temple));
+
+            using (var foo = acity.Residents.GetEnumerator())
+            {
+                foo.MoveNext();
+                var citizenTypes = foo.Current;
+                // initial state
+                Assert.Equal(0, citizenTypes.happy);
+                Assert.Equal(2, citizenTypes.content);
+                Assert.Equal(2, citizenTypes.unhappy);
+                Assert.Equal(1, citizenTypes.elvis);
+
+                foo.MoveNext();
+                citizenTypes = foo.Current;
+                // luxury
+                Assert.Equal(1, citizenTypes.happy);
+                Assert.Equal(1, citizenTypes.content);
+                Assert.Equal(2, citizenTypes.unhappy);
+                Assert.Equal(1, citizenTypes.elvis);
+
+                foo.MoveNext();
+                citizenTypes = foo.Current;
+                // temple
+                Assert.Equal(1, citizenTypes.happy);
+                Assert.Equal(2, citizenTypes.content);
+                Assert.Equal(1, citizenTypes.unhappy);
+                Assert.Equal(1, citizenTypes.elvis);
+            }
+
+        }
+
+        [Fact]
+        public void City5Colosseum()
+        {
+            var unit = Game.Instance.GetUnits().First(x => x.Owner == playa.Civilization.Id);
+            City acity = Game.Instance.AddCity(playa, 1, unit.X, unit.Y);
+            acity.Size = 5;
+            acity.ResetResourceTiles(); // setting city size doesn't allocate all resources
+
+            acity.AddBuilding(Reflect.GetBuildings().First(b => b is Colosseum));
+
+            using (var foo = acity.Residents.GetEnumerator())
+            {
+                foo.MoveNext();
+                var citizenTypes = foo.Current;
+                // initial state
+                Assert.Equal(0, citizenTypes.happy);
+                Assert.Equal(3, citizenTypes.content);
+                Assert.Equal(2, citizenTypes.unhappy);
+                Assert.Equal(0, citizenTypes.elvis);
+
+                foo.MoveNext();
+                citizenTypes = foo.Current;
+                // luxury
+                Assert.Equal(0, citizenTypes.happy);
+                Assert.Equal(3, citizenTypes.content);
+                Assert.Equal(2, citizenTypes.unhappy);
+                Assert.Equal(0, citizenTypes.elvis);
+
+                foo.MoveNext();
+                citizenTypes = foo.Current;
+                // Colosseum
+                Assert.Equal(0, citizenTypes.happy);
+                Assert.Equal(5, citizenTypes.content);
+                Assert.Equal(0, citizenTypes.unhappy);
+                Assert.Equal(0, citizenTypes.elvis);
+            }
+
+        }
     }
 }
--- xunit/src/TestsBase.cs
+++ xunit/src/TestsBase.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿// CivOne
+//
+// To the extent possible under law, the person who associated CC0 with
+// CivOne has waived all copyright and related or neighboring rights
+// to CivOne.
+//
+// You should have received a copy of the CC0 legalcode along with this
+// work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+// Author: Kevin Routley : July, 2019
+
+using System;
 using System.Linq;
 using System.Threading;
 using CivOne.UnitTests;
